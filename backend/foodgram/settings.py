f71c7e94bc92da4a122f721246ee9a1768a5057e
Aisylu Khaidarova <aiskhaidarova@yandex-team.ru>
2022-07-19 01:36:16 +0300

    fix settings.py database2

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -65,14 +65,13 @@ WSGI_APPLICATION = 'foodgram.wsgi.application'
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 
 DATABASES = {
-    "default": {
-        "ENGINE": os.getenv("DB_ENGINE",
-                            default="django.db.backends.postgresql"),
-        "NAME": os.getenv("DB_NAME", default=None),
-        "USER": os.getenv("POSTGRES_USER", default=None),
-        "PASSWORD": os.getenv("POSTGRES_PASSWORD", default=None),
-        "HOST": os.getenv("DB_HOST", default=None),
-        "PORT": os.getenv("DB_PORT", default=None),
+    'default': {
+        'ENGINE': os.getenv('DB_ENGINE', default="django.db.backends.postgresql"),
+        'NAME': os.getenv('DB_NAME', default="postgres"),
+        'USER': os.getenv('POSTGRES_USER', default="postgres"),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', default="hiais94"),
+        'HOST': os.getenv('DB_HOST', default="db"),
+        'PORT': os.getenv('DB_PORT', default="5432")
     }
 }
 
